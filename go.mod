6b8619cbe62cbd7a9f4a720311ce265f80f597ba
Austin Armbruster <austin@iits.me>
2021-10-04 11:40:00 -0400

    Scans filesystem for conversions
    Scans the local directory for the files that end in warc.gz and
    produces a corresponding new line delineated JSON file.
    
    Produces a slightly different document including the IP included in the
    WARC file.

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/austinarmbruster/warc-to-json
 go 1.16
 
 require (
-	github.com/datatogether/warc v0.0.0-20190806125150-74ef3f5ea69f // indirect
-	github.com/pborman/uuid v1.2.1 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/austinarmbruster/warc v0.0.0-20211004150656-96250af68ec8
+	github.com/sergi/go-diff v1.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/fs"
+	"io/ioutil"
 	"log"
 	"math"
 	"os"
+	"strings"
 	"time"
 
-	"github.com/datatogether/warc"
+	"github.com/austinarmbruster/warc"
 )
 
 func main() {
@@ -24,6 +27,7 @@ func main() {
 type webData struct {
 	Timestamp time.Time         `json:"@timestamp"`
 	URI       string            `json:"uri"`
+	IP        string            `json:"ip"`
 	Headers   map[string]string `json:"headers"`
 	Info      string            `json:"info"`
 	Content   string            `json:"content"`
@@ -56,20 +60,65 @@ func (e *esbulk) MarshalJSON() ([]byte, error) {
 }
 
 func run() error {
+
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		err := handleWARCFile(file)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func handleWARCFile(file fs.FileInfo) error {
+	if file.IsDir() {
+		return nil
+	}
+
+	if !strings.HasSuffix(file.Name(), "warc.gz") {
+		return nil
+	}
+
+	fin, err := os.Open(file.Name())
+	if fin != nil {
+		defer fin.Close()
+	}
+	if err != nil {
+		return err
+	}
+
+	zr, err := gzip.NewReader(fin)
+	if err != nil {
+		return err
+	}
+
+	outputFile := fmt.Sprintf("%s.ndjson", file.Name())
+	fout, err := os.Create(outputFile)
+	if fout != nil {
+		defer fout.Close()
+	}
+	if err != nil {
+		return err
+	}
+
+	err = warcToNDJSON(fout, zr)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func warcToNDJSON(w io.Writer, r io.Reader) error {
 	numRecords := math.MaxInt32
 	index := "sample-data"
 
-	f, err := os.Open("/Users/austinarmbruster/git/web-data-loading/CC-MAIN-20210723143921-20210723173921-00000.warc.gz")
-	if err != nil {
-		return err
-	}
-
-	zr, err := gzip.NewReader(f)
-	if err != nil {
-		return err
-	}
-
-	wr, err := warc.NewReader(zr)
+	wr, err := warc.NewReader(io.NopCloser(r))
 	if err != nil {
 		return err
 	}
@@ -78,7 +127,6 @@ func run() error {
 	for i := 0; i < numRecords; i++ {
 		rec, err = wr.Read()
 		if err == io.EOF {
-			log.Printf("eof")
 			return nil
 		}
 		if err != nil {
@@ -98,13 +146,13 @@ func run() error {
 			if err != nil {
 				return err
 			}
-			fmt.Println(string(jBytes))
+			fmt.Fprintf(w, "%s\n",string(jBytes))
 
 			jBytes, err = json.Marshal(&wd)
 			if err != nil {
 				return err
 			}
-			fmt.Println(string(jBytes))
+			fmt.Fprintf(w, "%s\n",string(jBytes))
 		}
 	}
 
@@ -119,6 +167,8 @@ func convert(rec warc.Record) (*webData, error) {
 		switch k {
 		case "WARC-Target-URI":
 			wd.URI = v
+		case "WARC-IP-Address":
+			wd.IP = v
 		case "Content-Type":
 			wd.Headers[k] = v
 		case "Content-Length":
